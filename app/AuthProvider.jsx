d5a2d76ee16b872d0e4d1b72ba65a1aca9384f97
muhammedrishadpe <rishadpe@gmail.com>
2025-03-25 11:24:31 +0100

    Added user save function and React context hook

--- app/AuthProvider.jsx
+++ app/AuthProvider.jsx
@@ -1,13 +1,15 @@
 "use client"
 import { ConvexProvider } from 'convex/react'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useMutation } from 'convex/react'
 import { useUser } from '@stackframe/stack'
 import { api } from "../convex/_generated/api"; 
+import { UserContext } from './_context/UserContext'
 
 function AuthProvider({children}) {
     const user=useUser();
     const CreateUser=useMutation(api.users.CreateUser);
+    const [userData, setUserData] =  useState();
     useEffect(() => {
         console.log(user);
         user && CreateNewUser();
@@ -16,10 +18,14 @@ function AuthProvider({children}) {
     const CreateNewUser=async()=> {
         const result = await CreateUser({name:user?.displayName, email:user.primaryEmail});
         console.log(result);
+        setUserData(result)
     }
   return (
     <div>
+      <UserContext.Provider value={{userData,setUserData}}>
       {children} 
+      </UserContext.Provider>
+     
     </div>
   )
 }
