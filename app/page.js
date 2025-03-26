c23d36f454d143e03831ecd5c6c1ac8824adec74
muhammedrishadpe <rishadpe@gmail.com>
2025-03-26 12:14:29 +0100

    added userButton to page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
+import { UserButton } from "@stackframe/stack";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (<div><h1>Halloword</h1></div>);
+  return (<div><h1>Halloword</h1>
+  <Button variant={'destructive'}>Subscribe</Button>
+  <UserButton/>
+  </div>);
+
+  
 }
