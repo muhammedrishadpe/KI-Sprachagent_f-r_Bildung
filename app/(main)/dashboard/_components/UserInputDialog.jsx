d297d66385b041fbeaeea719fba73691ba4f302f
muhammedrishadpe <rishadpe@gmail.com>
2025-04-02 19:52:28 +0200

    fixed miner bug

--- app/(main)/dashboard/_components/UserInputDialog.jsx
+++ app/(main)/dashboard/_components/UserInputDialog.jsx
@@ -33,22 +33,23 @@ const [selectExpert, setSelectExpert] = useState();
                 Gib ein Thema ein, um deine Fähigkeiten zu meistern{" "}
                 {coachingOption.name}
               </h1>
-              <div key={index} onClick={()=> setSelectExpert(expert.name)}>
-                className={`${selectExpert === expert.name && 'border-2'} p-1 rounded-2xl`}
-                {CoachingExpert.map((expert, index) => (
-                 <div className='grid grid-cols-3 md:grid-cols-5 gap-6  mt-3'>
-                   <Image
-                    src={expert.avatar}
-                    alt={expert.name}
-                    width={100}
-                    height={100}
-                    className={`rounded-2xl h-[80px] w-[80px]  object-cover hover:scale-105 transition-all cursor-pointer`}
-                  />
-                  <h2 className="text-center">{expert.name}</h2>
-                 </div>
-                ))}
-              </div>
-            </div>
+  <div className='grid grid-cols-3 md:grid-cols-5 gap-6  mt-3'>
+  {CoachingExpert.map((expert, index) => (
+    <div key={index} onClick={()=> setSelectExpert(expert.name)}
+    className={`${selectExpert === expert.name && 'border-2'} p-1 rounded-2xl`}
+  >              
+      <Image
+        src={expert.avatar}
+        alt={expert.name}
+        width={100}
+        height={100}
+        className={`rounded-2xl h-[80px] w-[80px]  object-cover hover:scale-105 transition-all cursor-pointer ${selectExpert === expert.name && 'border-2'}`}
+      />
+      <h2 className="text-center">{expert.name}</h2>
+    </div>
+  ))}
+  </div>
+</div>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
